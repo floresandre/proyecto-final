1d40a1c1ddbe216202eed51eed85c3e9e0d0a9cb
andre flores <flores_andre93@hotmail.com>
2024-07-11 21:13:44 -0300

    proyecto final

---
+++ js/main.js
@@ -0,0 +1,163 @@
+
+const btnCart = document.querySelector('.container-cart-icon');
+const containerCartProducts = document.querySelector(
+	'.container-cart-products'
+);
+
+btnCart.addEventListener('click', () => {
+	containerCartProducts.classList.toggle('hidden-cart');
+});
+
+
+const cartInfo = document.querySelector('.cart-product');
+const rowProduct = document.querySelector('.row-product');
+
+const productsList = document.querySelector('.container-items');
+
+let allProducts = [];
+
+const valorTotal = document.querySelector('.total-pagar');
+
+const countProducts = document.querySelector('#contador-productos');
+
+const cartEmpty = document.querySelector('.cart-empty');
+const cartTotal = document.querySelector('.cart-total');
+
+productsList.addEventListener('click', e => {
+	if (e.target.classList.contains('btn-add-cart')) {
+		const product = e.target.parentElement;
+
+		const infoProduct = {
+			quantity: 1,
+			title: product.querySelector('h2').textContent,
+			price: product.querySelector('p').textContent,
+		};
+
+		const exits = allProducts.some(
+			product => product.title === infoProduct.title
+		);
+
+		if (exits) {
+			const products = allProducts.map(product => {
+				if (product.title === infoProduct.title) {
+					product.quantity++;
+					return product;
+				} else {
+					return product;
+				}
+			});
+			allProducts = [...products];
+		} else {
+			allProducts = [...allProducts, infoProduct];
+		}
+
+		showHTML();
+	}
+});
+
+rowProduct.addEventListener('click', e => {
+	if (e.target.classList.contains('icon-close')) {
+		const product = e.target.parentElement;
+		const title = product.querySelector('p').textContent;
+
+		allProducts = allProducts.filter(
+			product => product.title !== title
+		);
+
+		console.log(allProducts);
+
+		showHTML();
+	}
+});
+
+
+const showHTML = () => {
+	if (!allProducts.length) {
+		cartEmpty.classList.remove('hidden');
+		rowProduct.classList.add('hidden');
+		cartTotal.classList.add('hidden');
+	} else {
+		cartEmpty.classList.add('hidden');
+		rowProduct.classList.remove('hidden');
+		cartTotal.classList.remove('hidden');
+	}
+
+	rowProduct.innerHTML = '';
+
+	let total = 0;
+	let totalOfProducts = 0;
+
+	allProducts.forEach(product => {
+		const containerProduct = document.createElement('div');
+		containerProduct.classList.add('cart-product');
+
+		containerProduct.innerHTML = `
+            <div class="info-cart-product">
+                <span class="cantidad-producto-carrito">${product.quantity}</span>
+                <p class="titulo-producto-carrito">${product.title}</p>
+                <span class="precio-producto-carrito">${product.price}</span>
+            </div>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="icon-close"
+            >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                />
+            </svg>
+        `;
+
+		rowProduct.append(containerProduct);
+
+		total =
+			total + parseInt(product.quantity * product.price.slice(1));
+		totalOfProducts = totalOfProducts + product.quantity;
+	});
+
+	valorTotal.innerText = `$${total}`;
+	countProducts.innerText = totalOfProducts;
+
+	
+};
+
+{const btn = document.querySelector('#blue-button')
+    btn.addEventListener('click', () => {
+    Swal.fire({
+        title: 'Genial!',
+        text: '¡Gracias por su compra!',
+        icon: 'success',
+        confirmButtonText: 'Aceptar'
+	
+    })
+})}
+
+const product = [ 
+	{ nombre: "Caja de herramientas", precio: 2000 },
+	{ nombre: "Kit de herramientas", precio: 20000 },
+	{ nombre: "Juego de pinzas", precio: 5000 },
+	{ nombre: "Serrucho", precio: 3000 },
+	{ nombre: "Taladro", precio: 30000 },
+
+];
+
+localStorage.setItem("product", JSON.stringify(product))
+
+const produtEnls = localStorage.getItem("product")
+
+
+fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+      .then(response => response.json())
+      .then(json => console.log(json))
+
+
+//tube un problema al querer vicular "datos.json" al feach no me elcontraba intente de varias maneras pero no encontre solucion por ahora.
+
+
+//fetch('./js/datos.json') es lo que intento y de varias maneras pero nohubo caso
+      
